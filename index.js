027374082074cfe360ee988c3b7ff94248d474e4
yanshoutong <yanshoutong@sina.cn>
2020-03-27 12:04:34 +0800

    test failed
    Signed-off-by: yanshoutong <yanshoutong@sina.cn>

--- index.js
+++ index.js
@@ -9,6 +9,55 @@ const js_beautify = require("./beautify").js_beautify;
 const raw = `let foobar= async res => {console.log('>>', res);
 return +new Date; }`;
 
+async function request_sync(options) {
+    return new Promise( (resolve, reject) => {
+        request(options, function(err, res, data) {
+            resolve({ err: err, res: res, data: data });
+        });
+    });
+}
+
+function is_url(uri) {
+    return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/.test(uri);
+}
+
+function terminate(msg = null) {
+    if (!!msg) console.error(msg);
+    yargs.showHelp();
+    process.exit(1);
+
+}
+
+async function get_source_codes() {
+    let uri = argv._[0];
+    let source_codes = null;
+
+    if (is_url(uri)) {
+        let { err, res, data } = await request_sync({
+            url: uri,
+            timeout: 5000,
+        });
+
+        if (!data) terminate(`failed to fetch ${uri}`);
+
+        source_codes = data;
+
+    } else {
+        try {
+            source_codes = fs.readFileSync(uri, { encoding: 'utf8' });
+        } catch(err) {
+            terminate(err.message());
+        }
+    }
+
+    if (!source_codes) {
+        terminate(`cannot get file content from ${uri}`);
+    }
+
+    return source_codes;
+
+}
+
 const argv = yargs
     .detectLocale(true)
     .usage('Usage: jsformat [options] file/url')
@@ -64,9 +113,30 @@ const argv = yargs
     })
     .argv;
 
-yargs.showHelp();
+if (argv._.length !== 1) {
+    terminate();
+}
 
+let source_codes = get_source_codes();
+let opt = {
+    indent_size: argv.indent_size,
+    indent_char: argv.indent_char,
+    preserve_newlines: argv.preserve_newlines,
+    max_preserve_newlines: argv.max_preserve_newlines,
+    jslint_happy: argv.jslint_happy,
+    brace_style: argv.brace_style,
+    space_before_conditional: argv.space_before_conditional,
+    unescape_strings: argv.unescape_strings,
+    wrap_line_length: argv.wrap_line_length,
+}
 
-function is_url(uri) {
-    return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/.test(uri);
+let output_codes = js_beautify(source_codes, opt);
+if (argv.output) {
+    try {
+        fs.writeFileSync(argv.output, output_codes);
+    } catch(err) {
+        terminate(`failed to write file to ${argv.output} due to ${err.message()}`);
+    }
+} else {
+    console.log(output_codes);
 }
