4b9624b425922437d6b38568f96491aa5118aed5
yanshoutong <yanshoutong@sina.cn>
2020-03-27 11:06:19 +0800

    cleanup codes
    Signed-off-by: yanshoutong <yanshoutong@sina.cn>

--- index.js
+++ index.js
@@ -9,34 +9,6 @@ const js_beautify = require("./beautify").js_beautify;
 const raw = `let foobar= async res => {console.log('>>', res);
 return +new Date; }`;
 
-console.log(js_beautify(raw));
-
-
-/*
- The options are:
-    indent_size (default 4)          - indentation size,
-    indent_char (default space)      - character to indent with,
-    preserve_newlines (default true) - whether existing line breaks should be preserved,
-    max_preserve_newlines (default unlimited) - maximum number of line breaks to be preserved in one chunk,
-
-    jslint_happy (default false) - if true, then jslint-stricter mode is enforced.
-
-            jslint_happy   !jslint_happy
-            ---------------------------------
-             function ()      function()
-
-    brace_style (default "collapse") - "collapse" | "expand" | "end-expand"
-            put braces on the same line as control statements (default), or put braces on own line (Allman / ANSI style), or just put end braces on own line.
-
-    space_before_conditional (default true) - should the space before conditional statement be added, "if(true)" vs "if (true)",
-
-    unescape_strings (default false) - should printable characters in strings encoded in \xNN notation be unescaped, "example" vs "\x65\x78\x61\x6d\x70\x6c\x65"
-
-    wrap_line_length (default unlimited) - lines should wrap at next opportunity after this number of characters.
-          NOTE: This is not a hard limit. Lines will continue until a point where a newline would
-                be preserved if it were present.
-*/
-
 const argv = yargs
     .detectLocale(true)
     .usage('Usage: jsformat [options] file/url')
